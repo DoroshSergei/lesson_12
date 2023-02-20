4bcfa1c49ef1c1e05195f99096babf6965093693
Sergei Dorosh <serega7031435@gmail.com>
2023-02-20 09:55:02 +0300

    add function

--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 main_blueprint = Blueprint('main_blueprint', __name__)
 
-@main_blueprint.route('/')
+
+@main_blueprint.route('/index.html')
 def main_page():
     return render_template('index.html')
+
+
+@main_blueprint.route('/search')
+def page_search_post():
+    s = request.args['s']
+    return render_template('post_list.html', s=s)
 
 
 
